631d84b0f69c40976be6692327480ec2914e7fb2
pablo <pa.nadinic@duocuc.cl>
2023-10-10 22:01:55 -0300

    otravez

--- src/app/perfiles/model/ClPerfil.ts
+++ src/app/perfiles/model/ClPerfil.ts
@@ -1,14 +1,28 @@
 // Definimos la Estructura de nuestro registro
 // Todos los campos son obligatorios 
 // al menos que se declare como opcional con signo de pregunta
-export interface ClPerfil{
+
+
+
+export class ClPerfil {
     // si no Inicializo los valores, da Error
     // Por eso es el constructor por obligación
-
-    id?:String
+    id:String
     fechanacimiento:String
-    //Signo pregunta, permite que el campo sea opcional}
+    //Signo pregunta, permite que el campo sea opcional
     usuario:String
     correo:String  
     clave:String
-}
+  
+  
+    // si no Inicializo los valores, da Error
+      constructor(obj: any){
+          this.id = obj && obj.id || null
+          this.usuario = obj && obj.usuario || null
+          this.fechanacimiento = obj && obj.fechanacimiento || null
+          this.correo = obj && obj.correo || null
+          this.clave = obj && obj.clave || null
+         
+          
+      }
+  }
