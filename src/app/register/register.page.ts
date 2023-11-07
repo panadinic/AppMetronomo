0a4c0c3533c745c5f3f29cd022857c428b1445c0
Pablocotal1 <pa.cotal@duocuc.cl>
2023-11-07 10:57:17 -0300

    arregloss

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -15,15 +15,15 @@ export class RegisterPage {
   fechaError: string = '';
   correo: string = '';
   usuarioError: string = '';
-  emailError: string= ''; 
+  emailError: string = '';
   email: string = '';
   error: string = '';
 
   mostrarContrasena: boolean = false;
 
-  constructor(private navCtrl: NavController, private toastCtrl: ToastController) {}
+  constructor(private navCtrl: NavController, private toastController: ToastController) {}
 
-  enviarRegistro() {
+  async enviarRegistro() {
     // Validación del usuario
     if (!this.usuario) {
       this.usuarioError = 'El usuario es obligatorio.';
@@ -32,67 +32,63 @@ export class RegisterPage {
     } else {
       this.usuarioError = '';
     }
-
-    
+  
     // Validación de fecha de nacimiento
     if (!this.fechaNacimiento) {
-      this.fechaError = 'La fecha de nacimiento es obligatorio.';
-    } else if (!/^[a-zA-Z]+$/.test(this.fechaNacimiento)) {
-      this.fechaError = 'La fecha de nacimiento debe contener solo numeros.';
+      this.fechaError = 'La fecha de nacimiento es obligatoria.';
+    } else if (!/^\d{4}-\d{2}-\d{2}$/.test(this.fechaNacimiento)) {
+      this.fechaError = 'La fecha de nacimiento debe tener el formato YYYY-MM-DD.';
     } else {
       this.fechaError = '';
     }
-
-
-    //validacion del email
-    if (!this.correo) {
+  
+    // Validación del email
+    if (!this.email) {
       this.emailError = 'El email es obligatorio.';
-    } else if (!this.validarFormatoEmail(this.correo)) {
+    } else if (!this.validarFormatoEmail(this.email)) {
       this.emailError = 'El email debe tener un formato válido.';
     } else {
       this.emailError = '';
     }
-    //validacion de la clave
-    if (!this.contrasena) {
-      this. contrasenaError = 'La clave es obligatoria.';
-    } else if (this.contrasena.length < 8) {
-      this. contrasenaError = 'La clave debe tener al menos 8 caracteres.';
   
+    // Validación de la contraseña
+    if (!this.contrasena) {
+      this.contrasenaError = 'La contraseña es obligatoria.';
+    } else if (this.contrasena.length < 8) {
+      this.contrasenaError = 'La contraseña debe tener al menos 8 caracteres.';
     } else if (!/[A-Z]/.test(this.contrasena)){
-      this. contrasenaError = 'La clave debe contener al menos una letra mayúscula.';
-
+      this.contrasenaError = 'La contraseña debe contener al menos una letra mayúscula.';
     } else if(!/[0-9]/.test(this.contrasena)){
-      this. contrasenaError = 'La clave debe contener al menos un numero.';
-
+      this.contrasenaError = 'La contraseña debe contener al menos un número.';
     } else if(!/[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(this.contrasena)){
-      this. contrasenaError = 'La clave debe contener al menos un caracter especial';
-    
+      this.contrasenaError = 'La contraseña debe contener al menos un carácter especial.';
     } else {
-      this. contrasenaError = '';
+      this.contrasenaError = '';
     }
-
-    
-    if (!this.usuario || 
-      !this.fechaNacimiento || 
-      !this.email || 
-      !this.contrasena || 
-      !this.recontrasena) {
+  
+    if (!this.usuario || !this.fechaNacimiento || !this.email || !this.contrasena || !this.recontrasena) {
       this.error = 'Todos los campos son obligatorios.';
     } else if (this.contrasena !== this.recontrasena) {
-      this.error = 'Las claves no coinciden.';
+      this.error = 'Las contraseñas no coinciden.';
     } else {
       // Realizar el proceso de registro aquí 
       this.error = ''; // Limpiar el mensaje de error
       // Redirigir a la página de éxito o hacer lo que sea necesario
-    }  
-
-    if (!this.usuarioError  && !this.emailError && !this.contrasena && !this.error) {
+      const toast = await this.toastController.create({
+        message: 'Registro exitoso',
+        duration: 2000, // Duración en milisegundos del mensaje
+        color: 'success', // Cambia el color a tu preferencia
+      });
+      toast.present();
+    }
+  
+    if (!this.usuarioError && !this.emailError && !this.contrasenaError && !this.error) {
       // Si no hay errores en ningún campo, redirigir a otra página
       this.navCtrl.navigateForward('/login'); // Cambia 'login' por la ruta de la página deseada
     }
   }
 
-   //validacion solo para el formato del email
+  // Validación solo para el formato del email
   validarFormatoEmail(email: string): boolean {
     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(email);
@@ -104,7 +100,4 @@ export class RegisterPage {
 
   ngOnInit() {
   }
-
 }
-
-
